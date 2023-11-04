61fc3e634d05ddf2b33771123c46f839b460aeeb
Alexander Vue <71350633+vuea@users.noreply.github.com>
2023-11-03 18:28:22 -0700

    CE-11 Create and fill in the text sections with placeholder text. CE-12 Add placeholder images to the home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import NavigationBar from "./components/navigationBar";
 import SearchBar from "./components/searchComponent";
+import SecondPage from "./components/imageTextBox";
+
 
 function App() {
   return <div>
     <NavigationBar />
     <SearchBar />
+    <SecondPage/>
   </div>;
 }
 
---
+++ src/components/imageTextBox.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import imgHolder from '../images/imageHold.jpeg';
+import Cube from '../images/cube.png';
+
+function ImageTextBox() {
+  const textBoxStyle = {
+    display: "flex",
+    backgroundColor: "#f3f3f3",
+    padding: "2%",
+    border: "1px solid #ddd",
+    maxWidth: "100%",
+    maxHeight: "100%", 
+  };
+  
+  const headStyle = {
+    marginTop: "8%",
+    marginLeft: "10%",
+    fontSize: "1vw",
+  };
+  
+  const bodyStyle = {
+    marginLeft: "10%",
+    fontSize: "3.5vw",
+  };
+  
+  const parStyle = {
+    marginLeft: "10%",
+    fontSize: "1.35vw",
+  };
+
+  const imgStyle = {
+    marginTop: "3%",
+    marginLeft: "5%",
+    marginBottom: "5%",
+    width: "40%", // Adjusted width to be more responsive
+    height: "auto", // Allows images to maintain aspect ratio
+    maxWidth: "100%",
+    maxHeight: "100%",
+  };
+  
+  const cubeStyle = {
+    marginLeft: "10%",
+    marginTop: "2.5%",
+    width: "7%",
+    height: "auto",
+    maxWidth: "100%",
+  };
+  
+  const secondCubeStyle = {
+    marginLeft: "40%",
+    marginTop: "0.5%",
+    width: "7%",
+    height: "auto",
+    maxWidth: "100%",
+  };
+  
+  const textCube = {
+    marginLeft: "10%", // Adjusted to a percentage for responsiveness
+    fontSize: "1.5vw",
+  };
+  
+  const secondTextCube = {
+    marginLeft: "57%", // Adjusted to a percentage for responsiveness
+    marginTop: "-8%", // Adjusted to a percentage for responsiveness
+    fontSize: "1.5vw",
+  };
+  
+  const paraText = {
+    marginLeft: "10%", // Adjusted to a percentage for responsiveness
+    fontSize: "0.7vw", // Adjusted font size to be more readable and responsive
+  };
+  
+  const secondParaText = {
+    marginLeft: "57%", // Adjusted to a percentage for responsiveness
+    marginTop: "1%", // Adjusted to a percentage for responsiveness
+    fontSize: "0.7vw", // Adjusted font size to be more readable and responsive
+  };
+  
+  const startButton = {
+    marginLeft: "10%", // Adjusted to a percentage for responsiveness
+    marginTop: "5%", // Adjusted to a percentage for responsiveness
+    width: "15%",
+    height: "6%",
+    fontSize: "0.7vw", // Adjusted font size to be responsive
+  };
+  
+  return (
+    <div style={textBoxStyle}>
+      <img style={imgStyle} src={imgHolder} alt="Image Holder" />
+      <div style={{ flex: "1", padding: "0 20px" }}>
+        <h1 style={headStyle}>Discover</h1>
+        <h1 style={bodyStyle}>Join Our Community And Help Others Advance Their Careers</h1>
+        <p style={parStyle}>
+          Rate my Externship is a platform built to help you explore a world of externship opportunities and make informed choices. Join our community and help others advance their careers by discovering the perfect externship for you and sharing your valuable externship experience with a supportive network of professionals.
+        </p>
+        <img style={cubeStyle} src={Cube} alt="Cube" />
+        <img style={secondCubeStyle} src={Cube} alt="Cube" />
+        <h1 style={textCube}>Find</h1>
+        <p style={paraText}>Find the Perfect Externship for You</p>
+        <h1 style={secondTextCube}>Share</h1>
+        <p style={secondParaText}>Share Your Valuable Externship Experiences with Others</p>
+        <button style={startButton}>Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default ImageTextBox;
