53c98ba75f39727bfd252fc9e7b5aa572a74ee41
Nazifullah Hanan <hananzai.1989@gmail.com>
2024-01-27 23:05:44 -0800

    CSS added for LogIn component

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -1,31 +1,33 @@
+import "./SignInForm.css";
+
 export default function SignInForm() {
   return (
-    <from>
-      <div>
-        <div>
-          <h1>Login</h1>
-          <p>Enter Your Account Details</p>
-        </div>
-
-        <p>
-          <label>Email</label>
-          <input type="email" required />
-        </p>
-
-        <div>
-          <label>Password</label>
-          <input type="password" required />
-          <button>Forgot Your Password?</button>
-        </div>
-        <div>
-          <button>Login</button>
-          <button>G Login with Google</button>
-        </div>
-        <span>
-          <p>Don't have an account?</p>
-          <button>Sign Up</button>
-        </span>
+    <div className="background  form-box">
+      <div className="box-border">
+        <from>
+          <div>
+            <h1>Login</h1>
+            <p id="form-p">Enter Your Account Details</p>
+          </div>
+          <div id="input-field">
+            <label>Email</label>
+            <input type="email" required />
+          </div>
+          <div id="input-field">
+            <label>Password</label>
+            <input type="password" required />
+            <button id="change-pass-btn">Forgot Your Password?</button>
+          </div>
+          <div id="log-in-btn">
+            <button id="login">Login</button>
+            <button id="login-google-btn">G Login with Google</button>
+          </div>
+          <span id="sign-up-btn">
+            <span>Don't have an account?</span>
+            <button>Sign Up</button>
+          </span>
+        </from>
       </div>
-    </from>
+    </div>
   );
 }
