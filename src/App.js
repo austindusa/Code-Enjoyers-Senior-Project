d763a0b8284cf847fc194bb623327148a47f22f6
Nazifullah Hanan <hananzai.1989@gmail.com>
2024-01-27 19:46:14 -0800

    sign in component created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
+// import "./App.css";
 import Home from "./home";
 import LogIn from "./login";
 import SignUp from "./signup";
 import ResultPage from "./pages/ResultPage";
 import WriteReview from "./components/WriteReview";
+import SignInForm from "./components/SignInForm.jsx";
 
 function App() {
-  return  <Router>
-    <div>
-    <Routes>
-      <Route path = "/" element = {<Home/>} />
-      <Route path = "/login" element = {<LogIn/>}/>
-      <Route path = "/signup" element = {<SignUp/>}/>
-      <Route path = "/resultpage" element = {<ResultPage/>}/>
-    </Routes>
-  </div>
-  </Router>
+  return (
+    <Router>
+      <div>
+        <SignInForm />
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/resultpage" element={<ResultPage />} />
+        </Routes> */}
+      </div>
+    </Router>
+  );
 }
 
 export default App;
