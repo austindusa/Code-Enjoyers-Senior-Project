5958a1614bb6012ef86b1f3f118ccc30b86d0d69
SWEbozo <oamartinez@csus.edu>
2024-02-09 14:50:17 -0800

    CE-56 Implemented a Review survey and added survey data as json file.

--- src/components/WriteReview.jsx
+++ src/components/WriteReview.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useState} from 'react'
-import {FaStar} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import SurveyComponent from './SurveyComponent'
 
 function WriteReview(props) {
 
@@ -26,7 +26,7 @@ function WriteReview(props) {
         display: 'inline-flex',
         width: '40.0625rem',
         height: '36.3125rem',
-        padding: '0.875rem 0.75rem',
+        padding: '0.75rem 0.75rem',
         flexDirection: 'column',
         alignItems: 'flex-start',
         gap: '0.625rem',
@@ -44,99 +44,15 @@ function WriteReview(props) {
         lineHeight: '140%',
     }
 
-    const writeReviewQuestionTextStyle = {
-        width: '35.75rem',
-        height: '1.75rem',
-        flexShrink: '0',
-        overflow: 'hidden',
-        color: '#000',
-        textOverflow: 'ellipsis',
-        whiteSpace: 'nowrap',
-        fontFamily: 'Roboto',
-        fontSize: '1rem',
-        fontStyle: 'normal',
-        fontWeight: '600',
-        lineHeight: '150%',
-    }
-
-    const starFrameStyle = {
-        display: 'flex',
-        width: '4.70313',
-        padding: '0.375rem 0.3125rem',
-        justifyContent: 'space-between',
-        alignItems: 'flex-start',
-    }
-
-    const [rating, setRating] = useState(null);
-
-    const[hover, setHover] = useState(null);
-
-    const starStyle = {
-        cursor: 'pointer',
-    }
-
-    const radioLabelStyle = {
-        color: 'var(--Black, #060606)',
-        fontFamily: 'Roboto',
-        fontSize: '0.625rem',
-        fontStyle: 'normal',
-        fontWeight: '400',
-        lineHeight: '150%',
-        gap: '0.75rem',
-    }
-
-    const radioButtonStyle = {
-        gap: '0.75rem',
-    }
-
-    const experienceStyle = {
-        display: 'flex',
-        width: '37.5rem',
-        height: '11.25rem',
-        padding: '0.75rem',
-        alignItems: 'flex-start',
-        flexShrink: '0',
-        resize: 'none',
-        margin: '1.0625rem 0rem',
-
-        borderRadius: '0.625rem',
-        border: '1px solid var(--Black, #060606)',
-        background: 'var(--White, #FFF)',
-    }
-
     const buttonContainer = {
         display: 'flex',
-        width: '10.3125rem',
+        width: '39rem',
+        height: '4.5rem',
         flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'flex-start',
+        alignItems: 'flex-end',
         gap: '0.625rem',
 
-        position: 'absolute',
-        right: '39.9375rem',
-        bottom: '11.8125rem',
-    }
-
-    const writeReviewBtnStyle = {
-        display: 'flex',
-        width: '4.9375rem',
-        height: '2.5305rem',
-        padding: '0.75rem 1.5rem',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: '0.5rem',
-
-        borderRadius: '9.375rem',
-        border: '1px solid var(--Black, #060606)',
-        background: 'var(--Black, #060606)',
-
-        color: 'var(--White, #FFF)',
-
-        fontFamily: 'Roboto',
-        fontSize: '0.625rem',
-        fontStyle: 'normal',
-        fontWeight: '400',
-        lineHeight: '150%',
     }
 
     const closeBtnStyle = {
@@ -145,10 +61,6 @@ function WriteReview(props) {
         justifyContent: 'center',
         alignItems: 'center',
         gap: '0.5rem',
-
-        position: 'absolute',
-        right: '-0.125rem',
-        top: '-0.0625rem',
 
         borderRadius: '9.375rem',
         border: '1px solid var(--Black, #060606)',
@@ -165,52 +77,23 @@ function WriteReview(props) {
     }
 
     return (props.trigger)? (
-
         <div className="writeReview" style = {writeReviewStyle}>
             <div className="writeReview-inner" style = {writeReviewInnerStyle}>
                 <div className="writeReview-text" style = {writeReviewTextStyle}>Adding New Review For:
                     <div className="writeReview-text" style = {writeReviewTextStyle}>Externship Title</div>
-                    <div className="writeReviewQuestion-text" style = {writeReviewQuestionTextStyle}>What rating would you give this externship?</div>
-                    <div className="starFrame" style = {starFrameStyle}>
-                        <div className="starRating">
-                            {[...Array(5)].map((star, index) => {
-                                const currentRating = index + 1;
-                                return (
-                                <label>
-                                    <input 
-                                        type="radio"
-                                        name="rating"
-                                        style={{display: "none"}}
-                                        value={currentRating}
-                                        onClick={() => setRating(currentRating)}
-                                    />
-                                    <FaStar className='star' style = {starStyle} size={15} color={currentRating <= (hover || rating) ? "#000" : "#EEEEE0"} onMouseEnter={() => setHover(currentRating)} onMouseLeave={() => setHover(null)} />
-                                </label>
-                                );
-                            })}
-                        </div>
+                    <div className="scrollableContainer" style={{ maxHeight: '450px', overflowY: 'auto' }}>           
+                        
+                        <SurveyComponent></SurveyComponent>
+
                     </div>
-                    <div className="writeReviewQuestion-text" style = {writeReviewQuestionTextStyle}>Were you paid for doing this externship?</div>
-                    <div className="yesNoButtons">
-                        <input type = "radio" name = "paid" style = {radioButtonStyle}/> <label style = {radioLabelStyle}>No</label>
-                        <input type = "radio" name = "paid" style = {radioButtonStyle}/> <label style = {radioLabelStyle}>Yes</label>
-                    </div>
-                    <div className="writeReviewQuestion-text" style = {writeReviewQuestionTextStyle}>Would you take this externship again if you could?</div>
-                    <div className="yesNoButtons">
-                        <input type = "radio" name = "again" style = {radioButtonStyle}/> <label style = {radioLabelStyle}>No</label>
-                        <input type = "radio" name = "again" style = {radioButtonStyle}/> <label style = {radioLabelStyle}>Yes</label>
-                    </div>
-                    <div className="writeReviewQuestion-text" style = {writeReviewQuestionTextStyle}>Please share your overall experience participating in this externship.</div>
-                    <textarea className="experience" style = {experienceStyle} placeholder='Type your message...'></textarea>
+
                     <div className="buttonContainer" style = {buttonContainer}>
-                        <Link to="/login"><button className="writeReview-btn" style = {writeReviewBtnStyle}>Add Review</button></Link>
                         <button className="close-btn" style = {closeBtnStyle} onClick={() => props.setTrigger(false)}>Cancel</button>
                         {props.children}
                     </div>
                 </div>
             </div>
         </div>
-
     ) : "";
 }
 
