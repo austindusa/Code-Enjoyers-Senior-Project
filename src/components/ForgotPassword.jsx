970ba449967573522091148e9e86768eceb08db6
Zabi Aryan <zabiullaharyan@yahoo.com>
2024-02-11 10:41:08 -0800

    sending email function completed. First commit

--- src/components/ForgotPassword.jsx
+++ src/components/ForgotPassword.jsx
@@ -1,14 +1,32 @@
+import {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import "./ForgotPassword.css";
 
 function ForgotPassword() {
+      const form = useRef();
 
-    return (
+      const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs
+        .sendForm('service_3hm1b1c', 'template_hz526oq', form.current, '4WEcz8x7HdeFoSZoT')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          }
+          
+        );
+        e.target.reset();
+      };
+   
+return (
     <div className="background-img">
       <div className="background form-box">
         <div className="box-border-fp">
-          <form>
+          <form ref={form} onSubmit={sendEmail}>
             <div>
                 <h1>Forgot Password</h1>
                 <p id="form-p">Please Enter Your Email Address:</p>
