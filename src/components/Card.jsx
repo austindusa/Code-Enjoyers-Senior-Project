d75d8a0a7d39a75fa931dd3df0be0f192167825d
Nazifullah Hanan <hananzai.1989@gmail.com>
2024-02-15 09:51:54 -0800

    Functionality added. First search result is now selected by default so that the detailed result window is displayed upon loading the page.In addition, Search result cards now CANNOT be deselected unless another card is selected.

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,92 +1,95 @@
-import React, {useState} from 'react';
-import Icon from '../images/Avatar Image.jpg';
+import React, { useContext, useState } from "react";
+import Icon from "../images/Avatar Image.jpg";
 
-function Card({organizationName, externshipTitle, location, description, onClick}){
-    const [bColor, setBColor] = useState('#FFF');
+function Card({
+  organizationName,
+  externshipTitle,
+  location,
+  description,
+  onClick,
+  isSelected,
+}) {
+  const cardStyle = {
+    display: "flex",
+    width: "32.875rem",
+    height: "6.4375rem",
+    padding: "0.5rem 0.5rem 0.5rem 0.5rem",
+    flexDirection: "column",
+    alignItems: "flex-start",
+    gap: "0.0625rem",
+    background: isSelected ? "#CCC" : "white",
+    boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+  };
 
-    const cardStyle = {
-        display: 'flex',
-        width: '32.875rem',
-        height: '6.4375rem',
-        padding: '0.5rem 0.5rem 0.5rem 0.5rem',
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        gap: '0.0625rem',
-        background: bColor,
-        boxShadow: '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
-    };
+  function handleClick() {
+    onClick();
+  }
 
-    const click = () => {
-        document.querySelectorAll('.card').forEach((card) => {card.style.background = '#FFF'});
-        setBColor((oldBColor) => (oldBColor === '#FFF' ? '#CCC' : '#FFF'));
-        onClick();
-    };
+  const horizontalContainer = {
+    display: "flex",
+    width: "11.625rem",
+    alignItems: "flex-start",
+    gap: "0.52125rem",
+  };
 
-    const horizontalContainer = {
-        display: 'flex',
-        width: '11.625rem',
-        alignItems: 'flex-start',
-        gap: '0.52125rem',
-    }
+  const imageStyle = {
+    width: "1.92188rem",
+    height: "1.875rem",
+  };
 
-    const imageStyle = {
-        width: '1.92188rem',
-        height: '1.875rem',
-    }
+  const h1Style = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    color: "#000",
+    fontFamily: "Inter",
+    fontSize: "0.875rem",
+    fontsStyle: "normal",
+    fontWeight: "600",
+    lineHeight: "1.25rem",
+    flexShrink: "0",
+    alignSelf: "stretch",
+  };
 
-    const h1Style = {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        color: '#000',
-        fontFamily: 'Inter',
-        fontSize: '0.875rem',
-        fontsStyle: 'normal',
-        fontWeight: '600',
-        lineHeight: '1.25rem',
-        flexShrink: '0',
-        alignSelf: 'stretch',
-    }
+  const h2Style = {
+    color: "#000",
+    fontFamily: "Inter",
+    fontSize: "1.125rem",
+    fontsStyle: "normal",
+    fontWeight: "600",
+    lineHeight: "1.5rem",
+  };
 
-    const h2Style = {
-        color: '#000',
-        fontFamily: 'Inter',
-        fontSize: '1.125rem',
-        fontsStyle: 'normal',
-        fontWeight: '600',
-        lineHeight: '1.5rem',
-    }
+  const locationTextStyle = {
+    color: "#000",
+    fontFamily: "Inter",
+    fontSize: "0.875rem",
+    fontsStyle: "normal",
+    fontWeight: "400",
+    lineHeight: "1.25rem",
+  };
 
-    const locationTextStyle = {
-        color: '#000',
-        fontFamily: 'Inter',
-        fontSize: '0.875rem',
-        fontsStyle: 'normal',
-        fontWeight: '400',
-        lineHeight: '1.25rem'
-    }
+  const descriptionStyle = {
+    color: "#000",
+    fontFamily: "Inter",
+    fontSize: "0.82713rem",
+    fontsStyle: "normal",
+    fontWeight: "600",
+    lineHeight: "1.25rem",
+    margin: "0rem",
+  };
 
-    const descriptionStyle = {
-        color: '#000',
-        fontFamily: 'Inter',
-        fontSize: '0.82713rem',
-        fontsStyle: 'normal',
-        fontWeight: '600',
-        lineHeight: '1.25rem',
-        margin: "0rem",
-    }
-
-    return(
-        <div className = "card" style={cardStyle} onClick = {() => click()}>
-            <div style={horizontalContainer}>
-                <img style ={imageStyle} src={Icon}/>
-                <text style={h1Style}>{organizationName}</text>
-            </div>
-            <text style={h2Style}>{externshipTitle}</text>
-            <text style={locationTextStyle}>{location}</text>
-            <p style={descriptionStyle}>{description}</p>
-        </div>
-    );
+  return (
+    <div className="card" style={cardStyle} onClick={handleClick}>
+      <div style={horizontalContainer}>
+        <img style={imageStyle} src={Icon} />
+        <text style={h1Style}>{organizationName}</text>
+      </div>
+      <text style={h2Style}>{externshipTitle}</text>
+      <text style={locationTextStyle}>{location}</text>
+      <p style={descriptionStyle}>{description}</p>
+    </div>
+  );
 }
 
 export default Card;
--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,48 +1,48 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import CardListWindow from "../components/CardListWindow";
 import ReviewContainer from "../components/reviewContainer";
 import NavigationBar from "../components/navigationBar";
 import ResultSearchBar from "../components/resultSearchComp";
 import BookmarkToggle from "../components/bookmarkToggle";
 
-function ResultPage(){
-    const [selectedCard, setSelected] = useState(null);
+function ResultPage() {
+  const [selectedCard, setSelected] = useState(null);
 
-    const horizontalStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '80%',
-        margin: 'auto',
-    }
+  const horizontalStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "80%",
+    margin: "auto",
+  };
 
-    const centerStyle = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column',
-        marginTop: '20px',
-      };
+  const centerStyle = {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    marginTop: "20px",
+  };
 
-    const handleCardClick = (cardData) => {
-        setSelected(cardData);
-    }
+  const handleCardClick = (cardData) => {
+    setSelected(cardData);
+  };
 
-    return(
-        <div>
-        <NavigationBar />
-        <div style = {horizontalStyle}>
+  return (
+    <div>
+      <NavigationBar />
+      <div style={horizontalStyle}>
         <ResultSearchBar />
-        <div style = {centerStyle}>
-        <BookmarkToggle />
+        <div style={centerStyle}>
+          <BookmarkToggle />
         </div>
-        </div>
-        <div style={horizontalStyle}>
-        <CardListWindow onCardClick = {handleCardClick}/>
-        {selectedCard && <ReviewContainer cardData = {selectedCard} />}
-        </div></div>
-    );
-
+      </div>
+      <div style={horizontalStyle}>
+        <CardListWindow onCardClick={handleCardClick} />
+        {selectedCard && <ReviewContainer cardData={selectedCard} />}
+      </div>
+    </div>
+  );
 }
 
-export default ResultPage; 
+export default ResultPage;
