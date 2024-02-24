3d78d8497b83f8690769532d9f8a17906396a8b9
austindusa <adusa@csus.edu>
2024-02-24 13:23:09 -0800

    CE-3 CE-83 Create a service.js file in the frontend to connect functions from the frontend to the controller methods in the backend
    Updated service.js file based on new information.

--- src/services/service.js
+++ src/services/service.js
@@ -3,10 +3,40 @@ import axios from "axios";
 const EXTERNSHIP_WEBSITE_URL = 'http://localhost:8080'
 class Service {
     createUser(user) {
-        return axios.post(EXTERNSHIP_WEBSITE_URL + '', user);
+        return axios.post(EXTERNSHIP_WEBSITE_URL + 'users');
     }
 
-    getUser() {
-        return axios.get(EXTERNSHIP_WEBSITE_URL + 'userId', userId);
+    getUser(userId) {
+        return axios.get(EXTERNSHIP_WEBSITE_URL + 'users/', userId);
+    }
+
+    updateUser(userId) {
+        return axios.put(EXTERNSHIP_WEBSITE_URL + 'users/', userId);
+    }
+
+    deleteUser(userId) {
+        return axios.delete(EXTERNSHIP_WEBSITE_URL + 'users/', userId);
+    }
+
+    createSurvey(survey) {
+        return axios.post(EXTERNSHIP_WEBSITE_URL + 'surveys/', survey);
+    }
+
+    getAllSurveys() {
+        return axios.get(EXTERNSHIP_WEBSITE_URL + 'results');
+    }
+
+    getSurvey(surveyId) {
+        return axios.get(EXTERNSHIP_WEBSITE_URL + 'results/', surveyId);
+    }
+
+    updateSurvey(surveyId) {
+        return axios.put(EXTERNSHIP_WEBSITE_URL + 'surveys/', surveyId);
+    }
+
+    deleteSurvey(surveyId) {
+        return axios.delete(EXTERNSHIP_WEBSITE_URL + 'surveys/', surveyId)
     }
 }
+
+export default new Service()
