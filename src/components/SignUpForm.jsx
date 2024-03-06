869315217c3f383942bd7b516a95dc476d1c3ccb
Nazifullah Hanan <hananzai.1989@gmail.com>
2024-03-05 19:43:57 -0800

    CE-100, a design has been created for the subsription service info page

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,79 +1,107 @@
 // Implement the log in functionality
-import { useState } from 'react';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase/config.js'; // Make sure this is the initialized instance
-import styles from './SignUpForm.module.css';
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase/config.js"; // Make sure this is the initialized instance
+import styles from "./SignUpForm.module.css";
 
 const SignUpForm = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [loginType, setLoginType] = useState('login');
+  const [loginType, setLoginType] = useState("login");
   const [userCredentials, setUserCredentials] = useState({});
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
-  function handleCredentiales(e){
-    setUserCredentials({...userCredentials, [e.target.name]: e.target.value});
-    
+  function handleCredentiales(e) {
+    setUserCredentials({ ...userCredentials, [e.target.name]: e.target.value });
   }
 
-  function handleSignup(e){
+  function handleSignup(e) {
     e.preventDefault();
     setError("");
-    createUserWithEmailAndPassword(auth, userCredentials.email, userCredentials.password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    setError(error.message);
- 
-    // ..
-  });
-}
-//  
+    createUserWithEmailAndPassword(
+      auth,
+      userCredentials.email,
+      userCredentials.password
+    )
+      .then((userCredential) => {
+        // Signed up
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        setError(error.message);
+
+        // ..
+      });
+  }
+  //
 
   return (
-    <div class="container"className={styles.signUpForm}>
-      <div><img src="path/to/logo.png" alt="Logo" className={styles.logo} /></div>
-      
+    <div class="container" className={styles.signUpForm}>
+      <div>
+        <img src="path/to/logo.png" alt="Logo" className={styles.logo} />
+      </div>
+
       <h2 className={styles.heading}>Sign Up</h2>
-      
-      <label htmlFor="email" className={styles.label}>Email</label>
-      <input onChange={(e)=>{handleCredentiales(e)}}
+
+      <label htmlFor="email" className={styles.label}>
+        Email
+      </label>
+      <input
+        onChange={(e) => {
+          handleCredentiales(e);
+        }}
         type="email"
-        name='email'
+        name="email"
         id="email"
         className={styles.input}
       />
 
-      <label htmlFor="password" className={styles.label}>Password</label>
-      <input onChange={(e)=>{handleCredentiales(e)}}
+      <label htmlFor="password" className={styles.label}>
+        Password
+      </label>
+      <input
+        onChange={(e) => {
+          handleCredentiales(e);
+        }}
         type="password"
-        name='password'
+        name="password"
         id="password"
         className={styles.input}
       />
 
-      <label htmlFor="confirmPassword" className={styles.label}>Confirm Password</label>
-      <input onChange={(e)=>{handleCredentiales(e)}}
+      <label htmlFor="confirmPassword" className={styles.label}>
+        Confirm Password
+      </label>
+      <input
+        onChange={(e) => {
+          handleCredentiales(e);
+        }}
         type="password"
-        name='confirmPassword'
+        name="confirmPassword"
         id="confirmPassword"
         className={styles.input}
       />
 
-      <button onClick={(e)=>{handleSignup(e)}} className={styles.button}>Sign Up</button>
+      <button
+        onClick={(e) => {
+          handleSignup(e);
+        }}
+        className={styles.button}
+      >
+        Sign Up
+      </button>
       <br></br>
-      {error &&
-        <div className="error">
-          {error}
-        </div>
-}
+      {error && <div className="error">{error}</div>}
 
-      <button className={`${styles.button} ${styles.googleSignUp}`}>Sign Up with Google</button>
+      <button className={`${styles.button} ${styles.googleSignUp}`}>
+        Sign Up with Google
+      </button>
       <br></br>
       <p className={styles.text}>
-        Already have an account? <a href="#" className={styles.link}>Log in</a>
+        Already have an account?{" "}
+        <a href="#" className={styles.link}>
+          Log in
+        </a>
       </p>
     </div>
   );
