4a990ac45ecbe816b769d0622b2e2430948b8acc
Nazifullah Hanan <hananzai.1989@gmail.com>
2024-04-05 23:17:27 -0700

    Issues for images stretching has been fixed

--- src/components/HeroSectionStyle.jsx
+++ src/components/HeroSectionStyle.jsx
@@ -3,31 +3,29 @@ import { colors } from "../colors";
 const styles = {
   container: {
     backgroundColor: colors.background,
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    height: '94.5vh',
-    position: 'relative',
-    paddingLeft: '7rem'
-    
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingLeft: "7rem",
   },
   textBox: {
-    justifyContent: 'column',
+    justifyContent: "column",
   },
   heading: {
     color: colors.text,
-    fontSize: '4rem',
+    fontSize: "4rem",
   },
   paragraph: {
     color: colors.text,
-    fontSize: '1.5rem',
-    marginBottom: '1rem',
+    fontSize: "1.5rem",
+    marginBottom: "1rem",
   },
   heroImage: {
-    position: 'relative',
-    width: '900',
-    height: '94.5vh',
+    position: "relative",
+    width: "100%",
+    height: "auto",
+    objectFit: "cover",
   },
   buttonContainerStyle: {
     paddingTop: ".5rem",
@@ -35,9 +33,7 @@ const styles = {
   actionButtonStyle: {
     backgroundColor: colors.primary,
     color: colors.text,
-  }
-  
-  
+  },
 };
 
 export default styles;
